dd8cc626c5996deb65d3b31f7304f145318f3213
王智阳 <wangzhiyang@ousatosBookpuro.fios-router.home>
2018-02-27 12:06:47 -0500

    mac-try

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -5,14 +5,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App container">
+        <div className="row">
+          <div className="col-sm-8">One of three columns </div>{' '}
+          <div className="col-sm-2">One of three columns </div>{' '}
+          <div className="col-sm-2">One of three columns </div>{' '}
+        </div>{' '}
       </div>
     );
   }
